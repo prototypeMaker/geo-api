55c621ca228930a124346320e8def3316f1a4cfe
Kevin Kamto <kamtokevin@gmail.com>
2020-02-01 13:37:17 -0500

    chore: simplified index.js + added geolocation

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const https = require('https');
 const express = require('express');
+const Geolocation = require('./Geolocation');
 let app = express();
 
 let port = process.env.PORT || '4202';
@@ -7,44 +8,13 @@ let host = process.env.HOST || 'http://localhost';
 
 app.listen(port, error => {
   console.log(`[SERVER] Live on ${host}:${port}`);
-  RecieveJson();
   if (error) throw new Error(error);
 });
 
 app.use(express.static('.'));
 
-function domainUpdate() {
-  console.log(`[SERVER] Posting to Google's Servers...`);
-  let username = process.env.USERNAME;
-  let password = process.env.PASSWORD;
-  let hostname = process.env.HOSTNAME;
-
-  if (!username || !password || !hostname)
-    throw new Error('Missing authentication input required');
-
-  let url = `https://${username}:${password}@domains.google.com/nic/update?hostname=${hostname}`;
-
-  const req = https
-    .request(url, res => {
-      console.log(`status - ${res.statusCode}`);
-      console.log(`output - ${res.complete}`);
-    })
-    .on('error', error => {
-      if (error) throw new Error(error);
-    })
-    .on('data', d => {
-      console.log(d);
-    });
-
-  req.end();
-}
-
-function RecieveJson()
-{
-  let IP =  `127.0.0.1`
-  let IPSTACK_ACCESSKEY = process.env.IPSTACK_ACCESSKEY;
-  let url = `http://api.ipstack.com/${IP}?access_key=${IPSTACK_ACCESSKEY}`
-}
+let Pi = new Geolocation();
+console.log(Pi.Payload)
 
 module.exports = app;
 
