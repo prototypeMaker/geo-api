7fc16bc0d5e38d76db16744169b8e3aeda2809f1
Donald Gatto <dgattoiii@gmail.com>
2020-05-13 18:23:00 -0400

    separated out app vs geolocation with props

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,23 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { withGoogleMap, GoogleMap } from "react-google-maps";
+import { GeoLocationMap } from "./GeoLocationMap";
+
+const coordinates = {
+  latitude: 36.214151845703125,
+  longitude: -81.67890930175781,
+};
 
 class App extends React.Component {
   render() {
-    const GoogleMapExample = withGoogleMap((props) => (
-      <GoogleMap
-        defaultCenter={{ lat: 40.756795, lng: -73.954298 }}
-        defaultZoom={13}
-      ></GoogleMap>
-    ));
-    return (
-      <div>
-        <GoogleMapExample
-          containerElement={<div style={{ height: `500px`, width: "500px" }} />}
-          mapElement={<div style={{ height: `100%` }} />}
-        />
-      </div>
-    );
+    return <GeoLocationMap coordinates={coordinates}></GeoLocationMap>;
   }
 }
 
