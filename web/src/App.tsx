4ff0bab5ea77abbb72dcc3432a75401706863035
Donald Gatto <dgattoiii@gmail.com>
2020-05-13 18:05:14 -0400

    google maps mvp working with react

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,9 +1,23 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { withGoogleMap, GoogleMap } from "react-google-maps";
 
 class App extends React.Component {
   render() {
-    return <div>Hello World</div>;
+    const GoogleMapExample = withGoogleMap((props) => (
+      <GoogleMap
+        defaultCenter={{ lat: 40.756795, lng: -73.954298 }}
+        defaultZoom={13}
+      ></GoogleMap>
+    ));
+    return (
+      <div>
+        <GoogleMapExample
+          containerElement={<div style={{ height: `500px`, width: "500px" }} />}
+          mapElement={<div style={{ height: `100%` }} />}
+        />
+      </div>
+    );
   }
 }
 
