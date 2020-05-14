8d3be925733477497f3547ad084b7aeffccb0f9d
Donald Gatto <dgattoiii@gmail.com>
2020-05-13 22:45:52 -0400

    getting api ready

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,24 +1,25 @@
-import { GeoLocation } from './Geolocation';
-import express from 'express';
+import { GeoLocation } from "./Geolocation";
+import express from "express";
 
 const app = express();
 
 const port = process.env.PORT || 4202;
-const host = process.env.HOST || 'http://localhost';
+const host = process.env.HOST || "http://localhost";
 
-const pi = new GeoLocation('152.10.249.31');
+// const pi = new GeoLocation('152.10.249.31');
 
 // Grabs GeoIP
-setTimeout(() => {
-  console.log(`${JSON.stringify(pi.getGeoIp(), null, 4)}`);
-}, 5000);
+// setTimeout(() => {
+//   console.log(`${JSON.stringify(pi.getGeoIp(), null, 4)}`);
+// }, 5000);
 
 app.listen(port, () => {
   console.log(`Listening on ${host}:${port}..`);
 });
 
-app.get('/', (req, res) => {
-  res.send('hello world');
+app.get("/", (req, res) => {
+  console.log("sending off!");
+  res.send("hello world");
 });
 
 module.exports = app;
