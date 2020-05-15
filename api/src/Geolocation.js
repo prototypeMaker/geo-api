d38dd87a2830d01cb01e2469e92f18d3bf741436
Kevin Kamto <kamtokevin@gmail.com>
2020-05-14 21:30:18 -0400

    fix: typescript compile errors

--- api/src/Geolocation.js
+++ api/src/Geolocation.js
@@ -35,15 +35,9 @@ var __generator = (this && this.__generator) || function (thisArg, body) {
         if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
     }
 };
-var __importStar = (this && this.__importStar) || function (mod) {
-    if (mod && mod.__esModule) return mod;
-    var result = {};
-    if (mod != null) for (var k in mod) if (Object.hasOwnProperty.call(mod, k)) result[k] = mod[k];
-    result["default"] = mod;
-    return result;
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-var http = __importStar(require("http"));
+exports.__esModule = true;
+exports.GeoLocation = void 0;
+var http = require("http");
 var GeoLocation = /** @class */ (function () {
     /**
      * @constructor Constructs instance of a device's GeoLocation information
