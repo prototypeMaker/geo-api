9d9ff9c5b7153adb3692f7627f1cb576ac8167d9
dgatto <dgattoiii@gmail.com>
2020-05-16 12:31:00 -0400

    verbiage changes

--- api/build/test/test.js
+++ api/build/test/test.js
@@ -5,17 +5,18 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 var chai_1 = __importDefault(require("chai"));
 var request_1 = __importDefault(require("request"));
-it('Main page content', function (done) {
-    var response = {
-        items: {
-            latitude: 36.214151845703125,
-            longitude: -81.67890930175781
-        }
-    };
-    var stringy = JSON.stringify(response);
-    console.log(stringy);
-    request_1.default('http://localhost:4202/', function (error, response, body) {
-        chai_1.default.expect(body).to.equal(stringy);
-        done();
+describe('GET /', function () {
+    it('should return coordinates', function (done) {
+        var response = {
+            items: {
+                latitude: 36.214151845703125,
+                longitude: -81.67890930175781
+            }
+        };
+        var stringy = JSON.stringify(response);
+        request_1.default('http://localhost:4202/', function (error, response, body) {
+            chai_1.default.expect(body).to.equal(stringy);
+            done();
+        });
     });
 });
--- api/test/test.ts
+++ api/test/test.ts
@@ -1,19 +1,20 @@
 import chai from 'chai';
 import request from 'request';
-import { json } from 'body-parser';
 
-it('should return GET /', function(done) {
-  const response = {
-    items: {
-      latitude: 36.214151845703125,
-      longitude: -81.67890930175781
-    }
-  };
+describe('GET /', () => {
+  it('should return coordinates', function(done) {
+    const response = {
+      items: {
+        latitude: 36.214151845703125,
+        longitude: -81.67890930175781
+      }
+    };
 
-  const stringy = JSON.stringify(response);
+    const stringy = JSON.stringify(response);
 
-  request('http://localhost:4202/', (error, response, body) => {
-    chai.expect(body).to.equal(stringy);
-    done();
+    request('http://localhost:4202/', (error, response, body) => {
+      chai.expect(body).to.equal(stringy);
+      done();
+    });
   });
 });
