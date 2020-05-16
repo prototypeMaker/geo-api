fc8f8a41f1398baf4f9a839c429e85169ef697f4
dgatto <dgattoiii@gmail.com>
2020-05-15 22:40:25 -0400

    prettier and token param

--- api/src/Particle.ts
+++ api/src/Particle.ts
@@ -2,36 +2,38 @@ import * as https from 'https';
 import { stringify } from 'querystring';
 import { Url } from 'url';
 import { IncomingMessage } from 'http';
+import { json } from 'body-parser';
 
-export class Particle
-{
-    private token: string | undefined;
-    private url: string;
-    constructor();
-    constructor($url?: string, $token?: string)
-    {
-        this.token = $token || process.env.TKNParticle;
-        this.url = $url || `https://api.particle.io/v1/devices`;
-        this.Authentication();
-    }
+export class Particle {
+  private url: string;
+  constructor();
+  constructor($url?: string) {
+    this.url = $url || `https://api.particle.io/v1/devices`;
+    this.authenticate();
+  }
 
-     Authentication($url?:string, $token?: string | any)
-     {
-         const options: string =  `${this.url}?access_token=${this.token}`;
-         https
-         .request(options, (res) => { 
-            let authResults = "none";
-            res.statusCode == 200 ? authResults = "success" : authResults = "failed"; 
-            console.log(`[Particle] HTTP ${res.statusCode}: Authentication ${authResults}`); 
-        })
-         .on("error", (error) => {console.log(`[Particle] Error attempting to Authentication + please check your API key`)})
-    }
+  authenticate($url?: string) {
+    const token = process.env.TKNParticle;
+    const options: string = `${this.url}?access_token=${token}`;
+    https
+      .request(options, res => {
+        let authResults = 'none';
+        res.statusCode == 200
+          ? (authResults = 'success')
+          : (authResults = 'failed');
+        console.log(
+          `[Particle] HTTP ${res.statusCode}: Authentication ${authResults}`
+        );
+      })
+      .on('error', error => {
+        console.log(
+          `[Particle] Error attempting to Authentication + please check your API key`
+        );
+      });
+  }
 
-    devices()
-    {
-        var url = `https://api.particle.io/v1/devices/`
-        this.Authentication(url);
-
-    }
-
+  devices() {
+    var url = `https://api.particle.io/v1/devices/`;
+    this.authenticate(url);
+  }
 }
