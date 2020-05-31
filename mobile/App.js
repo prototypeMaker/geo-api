41e3eff709e0a9350dc49e4163cd0f484fb74f1c
Kevin Kamto <kamtokevin@gmail.com>
2020-05-31 13:15:35 -0400

    feat: added gogle maps for mobile

--- mobile/App.js
+++ mobile/App.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import MapView from 'react-native-maps';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
+<MapView style={styles.container}
+initialRegion={{
+  latitude: 36.205958,
+  longitude: -81.671442,
+  latitudeDelta: 0.0922,
+  longitudeDelta: 0.0421,
+}}
+/>
   );
 }
 
