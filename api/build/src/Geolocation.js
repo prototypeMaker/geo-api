393fed4378429a1e4b27d19cb2fcb4622921ead5
Kevin Kamto <kamtokevin@gmail.com>
2020-06-10 23:35:37 -0400

    merge: incoming changes + local conflicts

--- api/build/src/Geolocation.js
+++ api/build/src/Geolocation.js
@@ -54,16 +54,6 @@ var __generator = (this && this.__generator) || function (thisArg, body) {
         if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
     }
 };
-var __importStar = (this && this.__importStar) || function (mod) {
-    if (mod && mod.__esModule) return mod;
-    var result = {};
-    if (mod != null) for (var k in mod) if (Object.hasOwnProperty.call(mod, k)) result[k] = mod[k];
-    result["default"] = mod;
-    return result;
-};
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.GeoLocation = void 0;
 var http = __importStar(require("http"));
